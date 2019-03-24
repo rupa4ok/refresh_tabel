aeadc4527693216c3cc0ef66f201f188c3f887d5
rupak <rupak4@yandex.ru>
2019-03-24 18:19:52 +0300

    1. Новый шаблон страницы авторизации

--- view/index.php
+++ view/index.php
@@ -1,43 +1,82 @@
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" lang="ru">
+<html lang="en">
 <head>
-    <title>Тестовое задание</title>
-    <link rel="stylesheet" type="text/css" href="../template/css/main.css">
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-            crossorigin="anonymous"></script>
+    <title>Login V4</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="/template/images/icons/favicon.ico"/>
+    <link rel="stylesheet" type="text/css" href="/template/vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/template/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="/template/fonts/iconic/css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" type="text/css" href="/template/vendor/animate/animate.css">
+    <link rel="stylesheet" type="text/css" href="/template/vendor/css-hamburgers/hamburgers.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="/template/vendor/animsition/css/animsition.min.css">
+    <link rel="stylesheet" type="text/css" href="/template/vendor/select2/select2.min.css">
+    <link rel="stylesheet" type="text/css" href="/template/vendor/daterangepicker/daterangepicker.css">
+    <link rel="stylesheet" type="text/css" href="/template/css/util.css">
+    <link rel="stylesheet" type="text/css" href="/template/css/main.css">
 </head>
 <body>
-<header>
 
-</header>
-
-<section class="main">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 offset-md-2">
-                <form method="POST" action="">
-                    <div class="form-row reg">
-                        <label for="name">Логин</label>
-                        <input type="text" class="form-control" pattern="[а-яА-Яa-zA-Z0-9_-]{3,15}"
-                               autofocus
-                               required title="Введите ваш логин" placeholder="Ваш логин" name="login">
-                        
-                        <label for="name">Пароль</label>
-                        <input type="password" class="form-control"
-                               pattern="[а-яА-Яa-zA-Z0-9_-]{6,100}" autofocus
-                               required title="Пароль не должен быть короче 6 символов" placeholder="Пароль"
-                               name="password">
-                        
-                        <input type="submit" class="btn btn-primary register" value="Войти">
+<div class="limiter">
+    <div class="container-login100" style="background-image: url('/template/images/bg-01.jpg');">
+        <div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
+            <form method="post" class="login100-form validate-form">
+					<span class="login100-form-title p-b-49">
+						Рефреш
+					</span>
+                
+                <div class="wrap-input100 validate-input m-b-23" data-validate = "Введите ваш логин">
+                    <span class="label-input100">Логин</span>
+                    <input class="input100" type="text" name="username" placeholder="Ваш логин" pattern="[а-яА-Яa-zA-Z0-9_-]{3,15}" autofocus>
+                    <span class="focus-input100" data-symbol="&#xf206;"></span>
+                </div>
+                
+                <div class="wrap-input100 validate-input" data-validate="Введите пароль">
+                    <span class="label-input100">Пароль</span>
+                    <input class="input100" type="password" name="pass" placeholder="Пароль">
+                    <span class="focus-input100" data-symbol="&#xf190;"></span>
+                </div>
+                
+                <div class="text-right p-t-8 p-b-31">
+                    <a href="#">
+                        Forgot password?
+                    </a>
+                </div>
+                
+                <div class="container-login100-form-btn">
+                    <div class="wrap-login100-form-btn">
+                        <div class="login100-form-bgbtn"></div>
+                        <button class="login100-form-btn">
+                            Войти
+                        </button>
                     </div>
-                </form>
-                <a href="reg.php">Зарегистрироваться</a>
-            </div>
+                </div>
+            </form>
         </div>
     </div>
-</section>
+</div>
+
+
+<div id="dropDownSelect1"></div>
+
+<!--===============================================================================================-->
+<script src="/template/vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+<script src="/template/vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+<script src="/template/vendor/bootstrap/js/popper.js"></script>
+<script src="/template/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+<script src="/template/vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+<script src="/template/vendor/daterangepicker/moment.min.js"></script>
+<script src="/template/vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+<script src="/template/vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+<script src="/template/js/main.js"></script>
+
 </body>
 </html>
